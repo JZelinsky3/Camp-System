b5848c365cce65a15d1c0c5bb7f120f0d862c713
shearappold <shearappold@gmail.com>
2022-10-19 17:37:52 -0400

    Description stub

---
+++ System/Description.java
@@ -0,0 +1,21 @@
+package System;
+
+public class Description {
+    private String firstName;
+    private String lastName;
+    private String birthdate;
+    private String gender;
+    private String hometown;
+
+    public Description(String firstName, String lastName, String birthdate, String gender, String hometown){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.birthdate=birthdate;
+        this.gender=gender;
+        this.hometown=hometown;
+    }
+
+    public String toString(){
+        return "";
+    }
+}
