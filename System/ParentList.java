f4a79940343bce8d6bf9e8ffaeed10e4bf9d1ab7
shearappold <shearappold@gmail.com>
2022-10-20 13:32:03 -0400

    ParentList stubs

---
+++ System/ParentList.java
@@ -0,0 +1,33 @@
+package System;
+
+import java.util.ArrayList;
+
+public class ParentList {
+    private ArrayList<Parent> parents= new ArrayList<Parent>;
+
+    private ParentList parentList;
+
+    private void ParentRecord(){
+        
+    }
+
+    public ParentList getInstance(){
+
+    }
+
+    public void addParent(Parent parent){
+
+    }
+
+    public Parent getParent(Parent parent){
+        return parent;
+    }
+
+    public void editParent(Parent parent){
+
+    }
+
+    public void saveParent(Parent parent){
+        
+    }
+}
