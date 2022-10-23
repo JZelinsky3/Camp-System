59c558a0959fffb2a50b32f80150af29cb6aec42
shearappold <shearappold@gmail.com>
2022-10-23 12:43:09 -0400

    CamperList Stub

---
+++ System/CamperList.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class CamperList {
+    
+    private ArrayList<Camper> campers=new ArrayList<Camper>();
+
+    private CamperList camperList;
+
+    private CamperList(){
+        
+    }
+
+    public CamperList getInstance(){
+        return camperList;
+    }
+
+    public void addCamper(Camper camper){
+
+    }
+
+    public Camper getCamper(Camper camper){
+        return camper;
+    }
+
+    public void editCamper(Camper camper){
+
+    }
+
+    public void saveCamper(Camper camper){
+        
+    }
+}
