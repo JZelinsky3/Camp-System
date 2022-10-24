7491c2ee983ada30592bb3ad6376df0f1ca63b86
Joe Zelinsky <zelinskj@email.sc.edu>
2022-10-24 17:31:00 -0400

    DataConstraints

--- System/DataConstants.java
+++ System/DataConstants.java
@@ -1,12 +1,46 @@
 package System;
 
-public abstract class DataConstants {
-	protected static final String USER_FILE_NAME = "src/users.json";
+public class DataConstants { 
+	protected static final String USERS_FILE_NAME = "system/json/users.json";
+	protected static final String CAMPERS_FILE_NAME = "system/json/campers.json";
+	protected static final String COUNSELORS_FILE_NAME = "system/json/counselors.json";
+	protected static final String SESSION_FILE_NAME = "system/json/sessions.json";
+	
+	protected static final String PARENTS = "parents";
+	protected static final String CAMPERS = "campers";
+	protected static final String COUNSELORS = "counselors";
+	protected static final String DIRECTOR = "director";
+	
 	protected static final String USER_ID = "id";
-	protected static final String USER_USER_NAME = "userName";
-	protected static final String USER_FIRST_NAME = "firstName";
-	protected static final String USER_LAST_NAME = "lastName";
-	protected static final String USER_AGE = "age";
-	protected static final String USER_PHONE_NUMBER = "phoneNumber";
+	protected static final String FIRST_NAME = "firstName";
+	protected static final String LAST_NAME = "lastName";
+	protected static final String USERNAME = "userName";
+	protected static final String PASSWORD = "password";
+	protected static final String EMAIL = "email";
+	protected static final String PHONE_NUMBER = "phoneNumber";
+	protected static final String BIRTHDAY = "birthday";
+	protected static final String ADDRESS = "address";
+	protected static final String EMERGENCY_CONTACTS = "emergencyContacts";
+	protected static final String GUARDIANS = "guardians";
+	protected static final String MEDICAL = "medicalInfo";
+	protected static final String DOCTOR = "physician";
+	protected static final String ALLERGIES = "allergies";
+	protected static final String MEDICATIONS = "details";
+	protected static final String DESCRIPTION = "description";
+	protected static final String TIME = "sinceExposure";
+	protected static final String DIRECTIONS = "directions";
+	protected static final String START_DATE = "startDate";
+	protected static final String END_DATE = "endDate";
+	protected static final String AGE_GROUP = "ageGroup";
+	protected static final String CABINS = "cabins";
+	protected static final String CABIN_AGE = "cabinAge";
+	protected static final String MAX_NO_OF_CAMPERS = "maxNumberOfCampers";
+	protected static final String SCHEDULES = "schedules";
+	protected static final String SESSION_DURATION = "sessionDuration";
+	protected static final String TITLE = "title";
+	protected static final String LOCATION = "location";
+	protected static final String START_TIME = "startTime";
+	protected static final String END_TIME = "endTime";
+	protected static final String SESSIONS = "sessions";
 }
  
