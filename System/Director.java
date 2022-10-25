271388495bd4572873bfa661e3eedb71918fbedb
shearappold <shearappold@gmail.com>
2022-10-25 13:41:21 -0400

    Director Stub

---
+++ System/Director.java
@@ -0,0 +1,41 @@
+package System;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Director {
+    
+    private ArrayList<Schedule> schedule=new ArrayList<Schedule>();
+
+    private ArrayList<Price> pricing=new ArrayList<Price>;
+
+    private ArrayList<Capacity> availability=new ArrayList<Capacity>();
+
+    private ArrayList<Expelled> expulsion=new ArrayList<Expelled>();
+
+    private ArrayList<Scholarship> scholarships=new ArrayList<Scholarship>;
+
+    public void addCamperSchedule(Schedule schedule){
+
+    }
+
+    public void addPrice(Price price){
+
+    }
+
+    public void addCapacity(Capacity capacity){
+
+    }
+
+    public void addExpelled(Expelled expelled){
+
+    }
+
+    public void removeExpelled(Expelled expelled){
+
+    }
+
+    public void addScholarships(Scholarships scholarships){
+        
+    }
+}
