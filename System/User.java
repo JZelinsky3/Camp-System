5cc773346f8976d3337ae98cf175e86d0e1e22e7
Joe Zelinsky <zelinskj@email.sc.edu>
2022-10-26 11:28:59 -0400

    user

--- System/User.java
+++ System/User.java
@@ -1,66 +1,88 @@
 package System;
 import java.util.UUID;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
 
 public class User {
-	private UUID id;
-	private String userName;
-	private String firstName;
-	private String lastName;
-	private int age;
-	private String phoneNumber;
-	
-	public User(String userName, String firstName, String lastName, int age, String phoneNumber) {
-		this.id = UUID.randomUUID(); 
-		this.userName = userName;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.age = age;
-		this.phoneNumber = phoneNumber;
-	}
-	
-	public User(UUID id, String userName, String firstName, String lastName, int age, String phoneNumber) {
-		this.id = id;
-		this.userName = userName;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.age = age;
-		this.phoneNumber = phoneNumber;
-	}
-	
-	public UUID getId() {
-		return id;
-	}
-	
-	public String getUserName() {
-		return userName;
-	}
-	
-	public String getFirstName() {
-		return firstName;
-	}
-	
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	
-	public String getLastName() {
-		return lastName;
-	}
-	
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	
-	public int getAge() {
-		return age;
-	}
-	
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	
+    protected UUID id;
+    protected String firstName; 
+    protected String lastName;
+    protected String userName;
+    protected String password;
+    protected String email;
+    protected String phoneNumber;
+    protected LocalDate birthday;
+    protected String address;
+    protected ArrayList<Camper> campers;
+
+    public User(String firstName, String lastName, String userName) {
+        this.firstName = firstName;
+        this.userName = userName;
+        this.lastName = lastName;
+    }
+
+    public User(UUID id, String firstName, String lastName, String userName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.userName = userName;
+        this.lastName = lastName;
+    }
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+    
+    public void addCampers(ArrayList<Camper> campers) {
+        this.campers = campers;
+    }
+
+    public void addPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void addEmail(String email) {
+        this.email = email;
+    }
+
+    public void addBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public void addCamper(Camper camper) {
+        this.campers.add(camper);
+    }
+
+    public void addAddress(String address) {
+        this.address = address;
+    }
+
+    private int calculateAge() {    
+        LocalDate currentDate = LocalDate.now();  
+        return Period.between(this.birthday, currentDate).getYears();
+    }
+
+    public void enrollCamper(Camper camper, Session session) {
+        //need more info on method
+        this.campers.add(camper);
+    }
+
+    public String toString() {
+        String print = "Name :"+this.firstName+" "+this.lastName+"\nUsername :"+this.userName+"\nPassword :"+this.password+"\nEmail: "+this.email+
+        "\nPhone Number: "+this.phoneNumber+"\nAddress: "+this.address+"\nType of account: " ;
+        for (int i = 0; i < campers.size(); i++) {
+			if (campers.get(i) != null) {
+                print += campers.get(i)+"\n";
+            }
+		}
+        return print;
+    }  
 }
