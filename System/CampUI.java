73d66bffc88fb61b6bc8dced6b40898701c61f81
Joe Zelinsky <zelinskj@email.sc.edu>
2022-10-27 13:32:49 -0400

    camp

--- System/CampUI.java
+++ System/CampUI.java
@@ -1,105 +1,214 @@
 package System;
 import java.util.Scanner;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class CampUI {
-	private static final String WELCOME_MESSAGE = "Welcome to Camp Congaree";
-	private String[] mainMenuOptions = {""};
 	private Scanner scanner;
-	private Camp camp;
-	
-	CampUI(){
-		scanner = new Scanner(System.in);
-		camp = new Camp();
-	}
-	
-	public void run() {
-		System.out.println(WELCOME_MESSAGE);
+    private SystemFACADE campFacade;
+
+    public CampUI(){
+        scanner = new Scanner(System.in);
+        campFacade = new SystemFACADE();
+    }
+
+    public void run(){
+        System.out.println("Welcome to our camp website!");
+        int choice;
+        boolean stop = false;
+        while(true){
+            displayIntroMenu();
+            choice = getUserCommand(2);
+            if(choice == -1){
+                System.out.println("Invalid input.");
+            }else if(choice == 1){
+                login();
+                break;
+            }else if(choice == 2){
+                createAccount();
+            }  
+        }
 		
-		//Loop as long as we want to keep interacting with the library
-		while(true) {
-			displayMainMenu();
-			
-			int userCommand = getUserCommand(mainMenuOptions.length);
-			
-			if(userCommand == -1) {
-				System.out.println("Not a valid command");
-				continue;
-			}
-			
-			//if they picked the last option then log them out
-			if(userCommand == mainMenuOptions.length -1) {
-				camp.logout();
-				break;
-			}
-		
-			switch(userCommand) {
+		while(!stop){
+            displayMainMenu();
+            choice = getUserCommand(6);
+            switch(choice){
 				case(0):
 					createAccount();
 					break;
 				case(1):
 					login();
 					break;
+				case (2):
+                    addCamper();
+                    break;
+                case (3):
+                    sessionSignup();
+                    break;
+                case (4):
+                    printGeneralInformation();
+                    break;
+                case (5):
+                    logout();
+                    stop = true;
+                    break;
+                default:
+                    System.out.println("Invalid input");
 			}
 		}
 		
 		System.out.println("Good bye, and have a nice day");
 		
 	}
-	
-	private void displayMainMenu() {
-		System.out.println("\n************ Main Menu *************");
-		for(int i=0; i< mainMenuOptions.length; i++) {
-			System.out.println((i+1) + ". " + mainMenuOptions[i]);
-		}
-		System.out.println("\n");
-	}
-	
-	//get the users command number, if it's not valid, return -1
-	private int getUserCommand(int numCommands) {
-		System.out.print("What would you like to do?: ");
-		
-		String input = scanner.nextLine();
-		int command = Integer.parseInt(input) - 1;
-		
-		if(command >= 0 && command <= numCommands -1) return command;
-		
-		return -1;
-	}
-	
-	private void createAccount() {
-		String userName = getField("Username");
-		String firstName = getField("First Name");
-		String lastName = getField("Last Name");
-		int age = Integer.parseInt(getField("Age"));
-		String phoneNumber = getField("Phone Number");
-		
-		if(camp.createAccount(userName, firstName, lastName, age, phoneNumber)) {
-			System.out.println("You have successfully created an account");
-		} else {
-			System.out.println("Sorry an account with that username already exists");
-		}
-	}
-	
-	private void login() {
-		String userName = getField("Username");
-		
-		if(camp.login(userName)) {
-			User currentUser = camp.getCurrentUser();
-			System.out.println("Welcome " + currentUser.getFirstName() + " " + currentUser.getLastName() + "!");
-		} else {
-			System.out.println("Sorry, invalid username ");
-		}
-	}
-	
-	private String getField(String prompt) {
-		System.out.print(prompt + ": ");
-		return scanner.nextLine();
-	}
-	
-	public static void main(String[] args) {
-		CampUI libraryInterface = new CampUI();
-		libraryInterface.run();
 
-	}
+	private void displayIntroMenu(){
+        System.out.println("Press (1) to sign in to your account");
+        System.out.println("Press (2) to create a new account");
+    }
 
+    private void displayMainMenu(){ //for choices: create child acct, add child to session
+        System.out.println("What would you like to do today? \n(1) add a new Camper \n(2) sign up camper for a session \n\n(3) General Information \n(5) Logout");
+    }
+
+    private int getUserCommand(int commands){
+        int choice = scanner.nextInt();
+        if(choice > 0 && choice <= commands){
+            return choice;
+        }
+        return -1;
+    }
+
+    private void createAccount(){
+        while(true){
+            System.out.print("\nEnter your first name: ");
+            String firstName = scanner.nextLine();
+            System.out.print("\nEnter your last name: ");
+            String lastName = scanner.nextLine();
+            System.out.print("\nEnter a username: ");
+            String username = scanner.nextLine();
+            System.out.print("\nEnter a password: ");
+            String password = scanner.nextLine();
+            System.out.print("\nEnter your email address: ");
+            String email = scanner.nextLine();
+            System.out.print("\nEnter your phone number: ");
+            String phoneNumber = scanner.nextLine();
+            System.out.print("\nEnter your birthday (format: yyyy-mm-dd): ");
+            String birthdayString = scanner.nextLine();
+            LocalDate birthday = LocalDate.parse(birthdayString);
+            System.out.print("\nEnter your address: ");
+            String address = scanner.nextLine();
+            if(campSystem.createAccount(username, password, email, lastName, firstName, phoneNumber, birthday, address) == true){
+                System.out.println("\nAccount created successfully.");
+                break;
+            }
+            System.out.println("\nInvalid input. Start over.");
+        }
+        
+    }
+
+    private void login(){
+        while(true){
+            System.out.print("Username: ");
+            String username = scanner.nextLine();
+            System.out.print("Password: ");
+            String password = scanner.nextLine();
+            if(campFacade.login(username, password) == true){
+                System.out.println("Login successful.");
+                break;
+            }
+            System.out.println("Username or password invalid. Try again.");
+        }
+    }
+
+    private void addCamper(){
+        while(true){
+            System.out.print("\nEnter the Campers first name: ");
+            String firstName = scanner.nextLine();
+            System.out.print("\nEnter the Campers last name: ");
+            String lastName = scanner.nextLine();
+            System.out.print("\nEnter Campers birthday (format: yyyy-mm-dd): ");
+            String birthdayString = scanner.nextLine();
+            LocalDate birthday = LocalDate.parse(birthdayString);
+            System.out.println("\n Enter the following information about the EMERGENCY CONTACT:");
+            Contact emergencyContact = createContact();
+            //maybe add guardians here (?)
+            //medical info starts here
+            System.out.println("\nEnter the following information about the Campers PHYSICIAN");
+            Contact physicianContact = createContact();
+            System.out.println("\nWould you like to add Treatments to your Campers Account (All necessary Treatments to be taken during camp must be added) Y/N");
+            String yn = scanner.nextLine();
+            ArrayList<Treatment> treatments;
+            while(yn.equalsIgnoreCase("y")){
+                System.out.print("\nWhat is the name of the Campers treatment");
+                String treatName = scanner.nextLine();
+                System.out.print("\nWhat time of day does the camper need to take medicine" );
+                String treatTime = scanner.nextLine();
+                Treatment treatment = new Treatment(treatName, treatTime);
+                treatments.add(treatment);
+                System.out.print("\nDo you want to add another treatment? (Y/N) ");
+                yn = scanner.nextLine();
+            }
+            //for adding allergies
+            System.out.println("\nWould you like to add any Allergies to your Campers Account (All necessary allergies to be taken during camp must be added) Y/N");
+            yn = scanner.nextLine();
+            ArrayList<String> allergies;
+            while(yn.equalsIgnoreCase("y")){
+                System.out.print("\nWhat is the campers allergy to?");
+                String allergy = scanner.nextLine();
+                allergies.add(allergy);
+                System.out.print("\nDo you want to add another allergy? Y/N");
+                yn = scanner.nextLine();
+            }
+
+    }
+
+    private Contact createContact(){
+        Contact contact;
+
+        System.out.print("\n Enter the first name: "); 
+        String firstName = scanner.nextLine();
+        System.out.print("\n Enter the last name: "); 
+        String lastName = scanner.nextLine();
+        System.out.print("\nEnter the address: ");
+        String address = scanner.nextLine();
+        System.out.print("\nEnter the phone number: ");
+        String phoneNumber = scanner.nextLine();
+        contact = new Contact(firstName, lastName, phoneNumber, address);
+        
+        return contact;
+    }
+
+
+    private void logout(){
+        System.out.println("Goodbye!");
+        System.exit(0);
+    }
+
+    private void sessionSignup(){
+        System.out.print("\nEnter the first name of the camper you would like to register for a session: ");
+        String firstName = scanner.nextLine();
+        System.out.print("\nEnter the last name of the camper you would like to register for a session: ");
+        String lastName = scanner.nextLine();
+        Camper camper = campFacade.findCamperByName(firstName, lastName);
+        Session session = chooseSession(camper);
+        campFacade.sessionSignup(camper, session);
+    }
+
+    private Session chooseSession(Camper camper){
+        int age = camper.getAge();
+        ArrayList<Session> options = campFacade.findAvailableSessions(age);
+        int counter = 1;
+        for(Session s : options){
+            System.out.println(counter + ") " + "Start: " + s.getStartDate() + ", End: " + s.getEndDate());
+        }
+        System.out.print("Choose a session by selecting a number: ");
+        int selection = scanner.nextInt();
+        return options.get(selection - 1);
+    }
+
+    public static void main(String[] args){
+        CampUI ui = new CampUI();
+        ui.run();
+    }
+    
 }
