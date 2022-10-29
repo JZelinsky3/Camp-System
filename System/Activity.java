2d2d5a9b5817fde1e8363990fbaa5dd9935cc30f
shearappold <shearappold@gmail.com>
2022-10-29 11:07:15 -0400

    Commit

--- System/Activity.java
+++ System/Activity.java
@@ -1,4 +1,5 @@
 package System;
+import java.time.Duration;
 import java.util.ArrayList;
 
 public class Activity {
@@ -8,7 +9,7 @@ public class Activity {
     private String endTime;
     private ArrayList<String> notes = new ArrayList<String>();
 
-    public Activity(String title, String location){
+    public Activity(String title, String location, String startTime, String endTime){
         this.title = title;
         this.location = location;
         this.startTime = startTime;
@@ -32,7 +33,18 @@ public class Activity {
     }
     
     public int calculateDuration(){
-        return 0;
+        int hourStringToInt=startTime.indexOf(":");
+        String hour=startTime.substring(0, hourStringToInt);
+        int hoursToMinutes=Integer.parseInt(hour)*60;
+        int minutesStringToInt=Integer.parseInt(startTime.substring(hourStringToInt));
+        int startTimeInt=hoursToMinutes+minutesStringToInt;
+        hourStringToInt=endTime.indexOf(":");
+        hour=endTime.substring(0, hourStringToInt);
+        hoursToMinutes=Integer.parseInt(hour)*60;
+        minutesStringToInt=Integer.parseInt(endTime.substring(hourStringToInt));
+        int endTimeInt=hoursToMinutes+minutesStringToInt;
+        int duration=endTimeInt-startTimeInt;
+        return duration;
     }
     
     public String toString(){
