b921703ff7dff3aac75740074accf78f2a86a606
Meet Patel <70076472+Meet7132002@users.noreply.github.com>
2022-11-01 23:39:13 -0400

    Parent Class

--- System/Parent.java
+++ System/Parent.java
@@ -1,31 +1,26 @@
-package System;
-
 import java.util.ArrayList;
-/**
- * Stores information on the parent of a camper
- */
+
 public class Parent {
-    
-    private ArrayList<Camper> children=new ArrayList<Camper>();
+    private ArrayList<Camper> children = new ArrayList<Camper>();
     private String firstName;
     private String lastName;
     private String username;
     private String password;
     private String email;
 
-    public Parent(String firstName, String lastName, String username, String password, String email){
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.username=username;
-        this.password=password;
-        this.email=email;
-    }
 
+    Parent(ArrayList<Camper> children,String firstName, String lastName,String username, String password, String email){
+        this.children = children;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
     public void register(Camper camper, Camp camp){
 
     }
-
     public void addCamper(Camper camper){
-        
+
     }
 }
